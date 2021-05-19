2a18bc267baa067e32cc122ffd2d3f9e66da7416
gsolde <gsolde@gmail.com>
2021-05-19 21:09:44 +0200

    feat: get rid of async search bar. No need.

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectCities } from "../../appSlice";
 import { EuiComboBox } from "@elastic/eui";
@@ -6,49 +6,18 @@ import "@elastic/eui/dist/eui_theme_amsterdam_light.css";
 
 function SearchBar() {
   const availableCities = useSelector(selectCities);
-
   const [selectedOptions, setSelected] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-  const [options, setOptions] = useState([]);
 
-  console.log(selectedOptions);
-
-  let searchTimeout;
   const onChange = (selectedOptions) => {
     setSelected(selectedOptions);
   };
 
-  const onSearchChange = useCallback(
-    (searchValue) => {
-      setLoading(true);
-      setOptions([]);
-
-      clearTimeout(searchTimeout);
-
-      searchTimeout = setTimeout(() => {
-        setLoading(false);
-        setOptions(
-          availableCities &&
-            availableCities.filter((option) => option.label.toLowerCase().includes(searchValue.toLowerCase()))
-        );
-      }, 500);
-    },
-    [availableCities]
-  );
-
-  useEffect(() => {
-    onSearchChange("");
-  }, [onSearchChange]);
-
   return (
     <EuiComboBox
       placeholder="Search City"
-      async
-      options={options}
+      options={availableCities}
       selectedOptions={selectedOptions}
-      isLoading={isLoading}
       onChange={onChange}
-      onSearchChange={onSearchChange}
     />
   );
 }
