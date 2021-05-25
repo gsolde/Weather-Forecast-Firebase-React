47f4d8cc244c95a11f9683c8604e8a602ef44b58
gsolde <gsolde@gmail.com>
2021-05-25 19:46:40 +0200

    style: button style toggle

--- src/components/weatherTile/WeatherTiles.js
+++ src/components/weatherTile/WeatherTiles.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { selectUser } from "../loginSignup/loginSignupSlice";
 import { selectWeatherReports } from "../searchBar/searchBarSlice";
 import { addCityToFavorites } from "../loginSignup/loginSignupSlice";
@@ -8,13 +8,17 @@ import { EuiButton, EuiCard, EuiFlexGrid, EuiFlexGroup, EuiFlexItem } from "@ela
 import "@elastic/eui/dist/eui_theme_amsterdam_light.css";
 
 function WeatherTiles() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
   const weatherReports = useSelector(selectWeatherReports);
   const user = useSelector(selectUser);
+
   const dispatch = useDispatch();
 
   function handleStoreFavorite(report, user) {
     pushFavoriteCityToFirebase(report, user);
     dispatch(addCityToFavorites(addFavoriteCityToStore(report)));
+    setIsFavorite(true);
   }
 
   return (
@@ -35,12 +39,12 @@ function WeatherTiles() {
                 <EuiFlexItem grow={false}>
                   <EuiButton
                     size="s"
-                    color="primary"
+                    color={isFavorite ? "danger" : "secondary"}
                     iconType="heart"
                     disabled={user ? false : true}
                     onClick={() => handleStoreFavorite(report, user)}
                   >
-                    Add to favorites
+                    {isFavorite ? "Delete from avorites!" : "Add to favorites"}
                   </EuiButton>
                 </EuiFlexItem>
               </EuiFlexGroup>
