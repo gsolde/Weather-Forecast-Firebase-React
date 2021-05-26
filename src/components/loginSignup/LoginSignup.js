aa1e56b075bbe20c4209fa406b54d3da7ae3ef12
gsolde <gsolde@gmail.com>
2021-05-26 17:34:39 +0200

    feat: firebase err handling

--- src/components/loginSignup/LoginSignup.js
+++ src/components/loginSignup/LoginSignup.js
@@ -11,6 +11,7 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [hasAccount, setHasAccount] = useState(true);
+  const [errMsg, setErrMsg] = useState(null);
 
   const dispatch = useDispatch();
   const history = useHistory();
@@ -24,13 +25,16 @@ function Login() {
   };
 
   function handleSignUp() {
-    auth.createUserWithEmailAndPassword(email, password);
-    //TODO handle errors!
+    auth.createUserWithEmailAndPassword(email, password).catch((err) => setErrMsg(err.message));
   }
 
   function handleLogin() {
-    auth.signInWithEmailAndPassword(email, password);
-    //TODO handle errors!
+    auth.signInWithEmailAndPassword(email, password).catch((err) => setErrMsg(err.message));
+  }
+
+  function handleLogInSignUpSwitch() {
+    setHasAccount(!hasAccount);
+    setErrMsg(null);
   }
 
   function authState() {
@@ -51,47 +55,50 @@ function Login() {
   }, []);
 
   return (
-    <div className={"loginFormContainer"}>
-      <EuiForm component="form">
-        <EuiFormRow label="e-mail">
-          <EuiFieldText placeholder="e-mail" value={email} onChange={(e) => onChangeEmail(e.target.value)} />
-        </EuiFormRow>
-        <EuiFormRow label="password">
-          <EuiFieldPassword
-            placeholder="Password"
-            type={"dual"}
-            value={password}
-            onChange={(e) => onChangePassword(e.target.value)}
-          />
-        </EuiFormRow>
+    <>
+      <div className={"loginFormContainer"}>
+        <EuiForm component="form">
+          <EuiFormRow label="e-mail">
+            <EuiFieldText placeholder="e-mail" value={email} onChange={(e) => onChangeEmail(e.target.value)} />
+          </EuiFormRow>
+          <EuiFormRow label="password">
+            <EuiFieldPassword
+              placeholder="Password"
+              type={"dual"}
+              value={password}
+              onChange={(e) => onChangePassword(e.target.value)}
+            />
+          </EuiFormRow>
+          <EuiSpacer />
+        </EuiForm>
+        {hasAccount ? (
+          <p className={"accountText"}>
+            Don't have an account yet?{" "}
+            <span className={"hasAccountSetter"} onClick={handleLogInSignUpSwitch}>
+              Sign up!
+            </span>
+          </p>
+        ) : (
+          <p className={"accountText"}>
+            If you have an account,{" "}
+            <span className={"hasAccountSetter"} onClick={handleLogInSignUpSwitch}>
+              Log in!
+            </span>
+          </p>
+        )}
         <EuiSpacer />
-      </EuiForm>
-      {hasAccount ? (
-        <p className={"accountText"}>
-          Don't have an account yet?{" "}
-          <span className={"hasAccountSetter"} onClick={() => setHasAccount(!hasAccount)}>
-            Sign up!
-          </span>
-        </p>
-      ) : (
-        <p className={"accountText"}>
-          If you have an account,{" "}
-          <span className={"hasAccountSetter"} onClick={() => setHasAccount(!hasAccount)}>
-            Log in!
-          </span>
-        </p>
-      )}
-      <EuiSpacer />
-      {hasAccount ? (
-        <EuiButton fill type="submit" color="primary" onClick={handleLogin}>
-          Log in
-        </EuiButton>
-      ) : (
-        <EuiButton fill type="submit" color="secondary" onClick={handleSignUp}>
-          Sign Up
-        </EuiButton>
-      )}
-    </div>
+        {hasAccount ? (
+          <EuiButton fill type="submit" color="primary" onClick={handleLogin}>
+            Log in
+          </EuiButton>
+        ) : (
+          <EuiButton fill type="submit" color="secondary" onClick={handleSignUp}>
+            Sign Up
+          </EuiButton>
+        )}
+        <div>{errMsg ? <p className="errorMsg">{errMsg}</p> : null}</div>
+      </div>
+    </>
   );
 }
 
