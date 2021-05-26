2a3c9ce4caf48bedfdd9420e3c124647d5a155a5
gsolde <gsolde@gmail.com>
2021-05-26 19:41:27 +0200

    chore: firebase config & homepage config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 import "firebase/auth";
 
 export const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC18HRzjmoNIWYPEgJjBIAWnaYfX4GJFDw",
+  authDomain: "iomedassignment.firebaseapp.com",
+  databaseURL: "https://iomedassignment-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "iomedassignment",
+  storageBucket: "iomedassignment.appspot.com",
+  messagingSenderId: "510675597698",
+  appId: "1:510675597698:web:2d23eb5d0774c1d596379b",
 });
 
 export const auth = firebaseApp.auth();
